33eab596830d84acf7f2e5afcd513605697755ab
Wang Chen <zhanghuilian-98022@163.com>
2018-12-01 14:46:44 +0800

    Minor change on index controller

--- src/com/fudan/sw/dsa/project2/controller/IndexController.java
+++ src/com/fudan/sw/dsa/project2/controller/IndexController.java
@@ -13,42 +13,35 @@ import com.fudan.sw.dsa.project2.service.IndexService;
  * For controller
  * controller is to get the http request, and give the http response
  * service is to do the business logic
- * @author zjiehang
  *
+ * @author zjiehang
  */
 @Controller
 @RequestMapping("/")
-public class IndexController 
-{
-	//use service to do all the business logic
-	@Autowired
-	IndexService indexService;
-	
-	@RequestMapping("/")
-	public String index()
-	{
-		indexService.createGraphFromFile();
-		return "index";
-	}
-	
-	/**
-	 * @params params: the attribute and its value
-	 * in params map, key is below,value is by UI
-	 * @param startAddress: the address of start point like 复旦大学张江校区
-	 * @param startlongitude: the longitude of start point
-	 * @param startLatitude: the latitude of start point
-	 * @param endAddress:  the address of end point like 复旦大学张江校区
-	 * @param endlongitude: the longitude of end point
-	 * @param endLatitude: the latitude of end point
-	 * @return
-	 */
-	@RequestMapping(value="/submitsearch",method={RequestMethod.POST,RequestMethod.GET})
-	@ResponseBody
-	public ReturnValue submitSearch(@RequestBody Map<String, Object> params)
-	{
+public class IndexController {
+    //use service to do all the business logic
+    @Autowired
+    IndexService indexService;
 
-		ReturnValue returnValue=indexService.travelRoute(params);
+    @RequestMapping("/")
+    public String index() {
+        indexService.createGraphFromFile();
+        return "index";
+    }
 
-		return returnValue;
-	}
+    /**
+     * @param params startAddress:   the address of start point like 复旦大学张江校区
+     *               startlongitude: the longitude of start point
+     *               startLatitude:  the latitude of start point
+     *               endAddress:     the address of end point like 复旦大学张江校区
+     *               endlongitude:   the longitude of end point
+     *               endLatitude:    the latitude of end point
+     *               in params map, key is below,value is by UI
+     * @return a ReturnValue object containing start point, end point and the subway lists.
+     */
+    @RequestMapping(value = "/submitsearch", method = {RequestMethod.POST, RequestMethod.GET})
+    @ResponseBody
+    public ReturnValue submitSearch(@RequestBody Map<String, Object> params) {
+        return indexService.travelRoute(params);
+    }
 }
