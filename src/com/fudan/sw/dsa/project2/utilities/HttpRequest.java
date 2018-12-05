1353a3919ad7523a22b3eee12d1a1efc1f75e6fb
Wang Chen <zhanghuilian-98022@163.com>
2018-12-05 21:41:23 +0800

    Get address info from map and print them out.

---
+++ src/com/fudan/sw/dsa/project2/utilities/HttpRequest.java
@@ -0,0 +1,59 @@
+package com.fudan.sw.dsa.project2.utilities;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+class HttpRequest {
+    /**
+     * Send a GET request to the specified URL
+     *
+     * @param param The parameters of the sending request, with different parameters
+     *              separated by symbol "&"
+     * @return URL The response given by the remote server
+     */
+    static String sendGet(String param) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            String urlNameString = "http://api.map.baidu.com/place/v2/search?" + param;
+            URL realUrl = new URL(urlNameString);
+            // Opens and closed the URL connection
+            URLConnection connection = realUrl.openConnection();
+            // Set a universal request header
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // Establish the real connection
+            connection.connect();
+            // Get all the request header fields
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // Iterate all the fields
+//            for (String key : map.keySet())
+//                System.out.println(key + "--->" + map.get(key));
+            // A BufferedReader InputStreamReader is used to read the request
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null)
+                result = result.concat(line);
+        } catch (Exception e) {
+            System.out.println("An error occurred while sending GET request." + e);
+            e.printStackTrace();
+        }
+        // finally block is used to close the stream
+        finally {
+            try {
+                if (in != null)
+                    in.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
---
+++ src/com/fudan/sw/dsa/project2/utilities/Main.java
@@ -0,0 +1,50 @@
+package com.fudan.sw.dsa.project2.utilities;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("stations.csv");
+        ArrayList<String> arrayList = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] stations = line.split("-");
+                AddStation:
+                for (String station : stations) {
+                    for (String existingStation : arrayList)
+                        if (existingStation.equals(station))
+                            continue AddStation;
+                    String query = "query=".concat(station);
+                    query = query.concat("&tag=公交车站&region=上海&output=xml&ak=KU0KWZsvYa9wr6cCQHEGYzNj8C3v4lDT");
+                    String xmlData = HttpRequest.sendGet(query);
+                    StringReader sr = new StringReader(xmlData);
+                    InputSource is = new InputSource(sr);
+                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    Document document = builder.parse(is);
+                    String lat = document.getElementsByTagName("lat").item(0).getFirstChild().getTextContent();
+                    String lng = document.getElementsByTagName("lng").item(0).getFirstChild().getTextContent();
+                    System.out.println(station + " " + lat + " " + lng);
+                    arrayList.add(station);
+                    Thread.sleep(1000);
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
