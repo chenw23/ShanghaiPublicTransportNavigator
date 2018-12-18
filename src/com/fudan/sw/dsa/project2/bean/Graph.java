fdbcbfa7cb2fdd3b02358031826f4305e5d509ad
Wang Chen <zhanghuilian-98022@163.com>
2018-12-18 19:33:57 +0800

    Added the data about the minimum transfer time so that when implementing the minimum transfer algorithm, the algorithm can change its policy according to the minimum transfer time known.

--- src/com/fudan/sw/dsa/project2/bean/Graph.java
+++ src/com/fudan/sw/dsa/project2/bean/Graph.java
@@ -353,4 +353,20 @@ public class Graph {
         walkingDistance = shortestWalkingDistance;
         return returnRoute(finalPath);
     }
+
+    private int getMinTransfer(@NotNull String lineA, @NotNull String lineB) {
+        int numberA = Integer.parseInt(lineA.substring(lineA.length() == 6 ? 5 : 6));
+        int numberB = Integer.parseInt(lineB.substring(lineB.length() == 6 ? 5 : 6));
+        if (numberA > 13) numberA -= 2;
+        if (numberB > 13) numberB -= 2;
+        final int[][] minTransfer = {{0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2},
+                {1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2},
+                {1, 1, 1, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2}, {1, 2, 1, 2, 0, 3, 2, 2, 2, 2, 2, 2, 2, 3, 3},
+                {2, 1, 1, 1, 3, 0, 1, 1, 1, 2, 1, 1, 2, 2, 2}, {1, 1, 1, 1, 2, 1, 0, 1, 1, 2, 2, 1, 1, 1, 2},
+                {1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 2, 2, 2}, {1, 1, 1, 1, 2, 1, 1, 1, 0, 2, 1, 1, 1, 2, 2},
+                {1, 1, 1, 1, 2, 2, 2, 1, 2, 0, 1, 1, 1, 2, 1}, {1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 0, 1, 1, 1, 2},
+                {1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 0, 1, 2, 2}, {1, 1, 1, 1, 2, 2, 1, 2, 1, 1, 1, 1, 0, 2, 2},
+                {2, 1, 2, 2, 3, 2, 1, 2, 2, 2, 1, 2, 2, 0, 2}, {2, 1, 2, 2, 3, 2, 2, 2, 2, 1, 2, 2, 2, 2, 0}};
+        return minTransfer[numberA][numberB];
+    }
 }
